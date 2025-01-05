3b35093e4bce0ac6ce59b971a85ebc8cf01a7377
HkrUchiwa2011 <fadilbouko780@gmail.com>
2025-01-05 08:56:55 +0100

    Delete commands/join.js

--- commands/join.js
+++
@@ -1,6 +0,0 @@
-module.exports = bot => {
-  bot.onText(/\/join/, async (msg) => {
-    const chatId = msg.reply_to_message.chat.id;
-    bot.unbanChatMember(chatId, msg.from.id); // Supposons que l'utilisateur soit temporairement banni.
-  });
-};
